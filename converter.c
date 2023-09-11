b78cacf0cb3a9bea2d3e3d58d054b7a4becb09aa
totarKun <totargaming@gmail.com>
2023-09-11 22:31:00 +0930

    create a handy program to run and test the converter

--- converter.c
+++ converter.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-float remit, vnd, exrate;
+float usd, vnd, exrate;
 int choice;
 int main(void) {
   float* ptrVND = &vnd;
-  float* ptrUSD = &remit;
+  float* ptrUSD = &usd;
   do {
     printf("Give me the rate from 1 unit to Vnd: ");
     scanf("%f", &exrate);
   } while (exrate < 1);
   do {
-    printf("choose the converter: \n(1) remit to VND\n(2) VND to remit");
+    printf("choose the converter: \n(1) usd to VND\n(2) VND to usd");
     scanf("%d", &choice);
 
   } while (choice != 1 && choice != 2);
   if (choice == 1) {
     do {
-      printf("\nEnter amount of money in remit currency:");
-      scanf("%f", &remit);
-    } while (remit < 0);
+      printf("\nEnter amount of money in usd currency:");
+      scanf("%f", &usd);
+    } while (usd < 0);
 
     printf("Amount of Money is %.3f\n", *ptrUSD * exrate);
   } else if (choice == 2) {
